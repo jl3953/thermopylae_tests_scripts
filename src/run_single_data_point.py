1181200838eadef6e41ab89971c843c6f33f303b
Jennifer Lam <jl87@princeton.edu>
2022-03-09 10:23:57 -0700

    do not continue smdbrpc command if the first one fails

--- src/run_single_data_point.py
+++ src/run_single_data_point.py
@@ -209,8 +209,8 @@ def prepromote_keys(
         ) for server_node in server_nodes]
     )
 
-    update_smdbrpc_repo = "cd /root/smdbrpc; git stash; git pull origin " \
-                          "demotehotkeys; /root/smdbrpc/generate_new_protos.sh"
+    update_smdbrpc_repo = "cd /root/smdbrpc && git stash && git pull origin " \
+                          "demotehotkeys && /root/smdbrpc/generate_new_protos.sh"
 
     cmd = "cd /root/smdbrpc/go; /usr/local/go/bin/go run {0} --batch={1} " \
           "--cicadaAddr={2} " \
