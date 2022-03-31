8ac9b0379d74f8de447260d661b2613240d95cab
Jennifer Lam <jl87@princeton.edu>
2022-03-31 00:21:18 -0400

    implement gathering of statistics for tpcc

--- src/gather.py
+++ src/gather.py
@@ -63,7 +63,7 @@ def aggregate(acc):
                 continue
 
     for k in final_datum:
-        if "ops" not in k:
+        if ("ops" not in k) and ("tpmC" not in k):
             final_datum[k] /= len(acc)
 
     return final_datum
@@ -93,5 +93,81 @@ def gather_data_from_raw_kv_logs(log_fpaths):
     return final_datum, True
 
 
+def extract_tpcc_data(lines):
+    def parse(header_line, data_line):
+
+        fields = data_line.strip().split()
+        suffix = ""
+        if fields[-1] in (
+            "delivery", "newOrder", "orderStatus", "payment", "stockLevel"):
+            suffix = "-{}".format(fields[-1])  # -delivery, -newOrder
+
+        header = [w + suffix for w in
+                  re.split('_+', header_line.strip().strip('_'))]
+        data = dict(zip(header, fields))
+        return data
+
+    lines.reverse()
+    reversed_lines = lines
+
+    types_of_txns_left = 5
+
+    datum = {}
+
+    prev_line = ""
+    for line in reversed_lines:
+        if types_of_txns_left == 0:
+            break
+
+        if "tpmC" in line:
+            continue
+        elif "Audit check" in line:
+            continue
+        elif len(line) == 0:
+            continue
+        elif "result" in line:
+            datum += parse(line, prev_line)
+        elif "__total" in line:
+            types_of_txns_left -= 1
+            if len(prev_line) > 0:
+                datum += parse(line, prev_line)
+        else:
+            prev_line = line
+
+    return datum
+
+
 def gather_data_from_raw_tpcc_logs(log_fpaths):
-    print(log_fpaths)
+    acc = []
+
+    for path in log_fpaths:
+        with open(path, "r") as f:
+            # read lines backwards in pairs
+
+            try:
+                datum = extract_tpcc_data(f.readlines())
+                acc.append(datum)
+            except BaseException as e:
+                print("failed to extract data: {0}, {1}".format(path, e))
+                return None, False
+
+    final_datum = aggregate(acc)
+    return final_datum, True
+
+
+def main():
+    log_fpaths = ['/root/thermopylae_tests_scripts/scratch'
+                  '/tpcc_throwaway_20220330_145105_840160/warehouses10'
+                  '/latency_throughput/logs/8_20220330_145105_854800/logs'
+                  '/bench_192.168.1.1.txt',
+                  '/root/thermopylae_tests_scripts/scratch'
+                  '/tpcc_throwaway_20220330_145105_840160/warehouses10'
+                  '/latency_throughput/logs/8_20220330_145105_854800/logs/'
+                  'bench_192.168.1.2.txt',
+                  '/root/thermopylae_tests_scripts/scratch'
+                  '/tpcc_throwaway_20220330_145105_840160/warehouses10'
+                  '/latency_throughput/logs/8_20220330_145105_854800/logs'
+                  '/bench_192.168.1.3.txt']
+    data = gather_data_from_raw_tpcc_logs(log_fpaths)
+    for k, v in data:
+        print(k, v)
