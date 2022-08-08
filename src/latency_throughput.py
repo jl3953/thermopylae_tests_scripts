3c77e4d6d5fb55b4f9240af484ff8ecc4eef6947
root <root@localhost>
2022-08-08 16:40:20 -0600

    delete commented lines

--- src/latency_throughput.py
+++ src/latency_throughput.py
@@ -46,76 +46,7 @@ def run(config, lt_config, log_dir):
     data = []
     #while step_size > 0:
     print("start", type(start), "end", type(end), "step_size", type(step_size))
-    concurrency_list = [i for i in range(8, 128, 8)]
-    # if config["skews"] == 0.01:
-    #     if "hot_node" in config:
-    #         if config["num_warm_nodes"] == 1:
-    #             concurrency_list = [24, 32, 36, 40, 48, 64, 80, 96, 120]
-    #         elif config["num_warm_nodes"] == 3:
-    #             concurrency_list = [20, 28, 36, 48, 56, 64, 72, 80, 96, 104]
-    #         elif config["num_warm_nodes"] == 7:
-    #             concurrency_list = [12, 16, 32, 40, 48, 56, 80]
-    #         elif config["num_warm_nodes"] == 15:
-    #             concurrency_list = [16, 24, 32, 48, 64, 80]
-    #     else:
-    #         if config["num_warm_nodes"] == 1:
-    #             concurrency_list = [8, 10, 12, 14, 16, 20, 24, 32]
-    #         elif config["num_warm_nodes"] == 2:
-    #             concurrency_list = [16, 20, 24, 28, 32, 36, 48, 52, 56]
-    #         elif config["num_warm_nodes"] == 4:
-    #             concurrency_list = [8, 16, 20, 24, 28, 32, 48, 56]
-    #         elif config["num_warm_nodes"] == 8:
-    #             concurrency_list = [2, 4, 6, 8, 10, 12, 14, 16, 20]
-    #         elif config["num_warm_nodes"] == 16:
-    #             concurrency_list = [8, 12, 16, 32, 48, 64, 80, 96]
-    # elif config["skews"] == 0.99:
-    #     if "hot_node" in config:
-    #         if config["num_warm_nodes"] == 1:
-    #             concurrency_list = [32, 40, 48, 56, 64, 80, 96, 120]
-    #         elif config["num_warm_nodes"] == 3:
-    #             concurrency_list = [40, 48, 56, 60, 64, 68, 72, 80, 96, 120]
-    #         elif config["num_warm_nodes"] == 7:
-    #             concurrency_list = [40, 48, 56, 64, 72, 80, 96, 120]
-    #         elif config["num_warm_nodes"] == 15:
-    #             concurrency_list = [16, 20, 24, 28, 32, 36, 40, 48, 56]
-    #     else:
-    #         if config["num_warm_nodes"] == 1:
-    #             concurrency_list = [8, 16, 20, 24, 28, 32, 40, 48, 56]
-    #         elif config["num_warm_nodes"] == 2:
-    #             concurrency_list = [2, 4, 8, 12, 16, 24, 28, 36]
-    #         elif config["num_warm_nodes"] == 4:
-    #             concurrency_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-    #         elif config["num_warm_nodes"] == 8:
-    #             concurrency_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    #         elif config["num_warm_nodes"] == 16:
-    #             concurrency_list = [1, 2, 3, 4, 5, 6, 7, 8]
-    # elif config["skews"] == 1.2:
-    #     if "hot_node" in config: # Thermopylae
-    #         if config["num_warm_nodes"] == 1:
-    #             concurrency_list = [40, 48, 56, 64, 72, 80, 88, 96, 120]
-    #         elif config["num_warm_nodes"] == 3:
-    #             concurrency_list = [40, 48, 56, 64, 72, 80, 88, 96, 120]
-    #         elif config["num_warm_nodes"] == 7:
-    #             concurrency_list = [36, 40, 48, 56, 64, 72, 80]
-    #         elif config["num_warm_nodes"] == 15:
-    #             concurrency_list = [16, 18, 20, 24, 28, 32, 40, 48]
-    #     else: # CRDB
-    #         if config["num_warm_nodes"] == 1:
-    #             concurrency_list = [4, 6, 7, 8, 9, 10, 12]
-    #         elif config["num_warm_nodes"] == 2:
-    #             concurrency_list = [4, 5, 6, 7,  8, 10, 12, 14]
-    #         elif config["num_warm_nodes"] == 4:
-    #             concurrency_list = [4, 6, 7, 8, 9, 10, 12, 16, 20]
-    #         elif config["num_warm_nodes"] == 8:
-    #             # one fourth the number of clients
-    #             concurrency_list = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
-    #         elif config["num_warm_nodes"] == 16:
-    #             # one fourth the number of clients
-    #             concurrency_list = [1, 2, 3, 4, 5, 6, 7, 8]
-    #
-    # else:
-    #     print("skew or num machines not set")
-    #     raise Exception("skew or num machines not set")
+    concurrency_list = [i for i in range(8, 16, 8)]
     for concurrency in concurrency_list:
         try:
             # run trial for this concurrency
@@ -135,8 +66,8 @@ def run(config, lt_config, log_dir):
             more_data = csv_utils.read_in_data(results_fpath_csv)
             datum.update(*more_data)
             data.append(datum)
-        except BaseException:
-            print("jenndebug move the fuck on")
+        except BaseException as err:
+            print("jenndebug move the fuck on", err)
 
     # find max throughput and hone in on it
     max_throughput_concurrency = max(data, key=operator.itemgetter("ops/sec(cum)"))["concurrency"]
